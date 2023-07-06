c6668413b39819ff20c28a9957d36ab8f70e4610
Oluwasegun Badaru <preeminentdeveloper@gmail.com>
2023-07-06 17:39:15 +0100

    web build

--- android/app/src/main/kotlin/com/example/daytrit/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.daytrit.io
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/utils/themes.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// color scheme
-ColorScheme colorScheme(BuildContext context) => Theme.of(context).colorScheme;
-TextTheme textTheme(BuildContext context) => Theme.of(context).textTheme;
